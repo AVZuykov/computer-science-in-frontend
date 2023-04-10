8b9726937e06db957cafd26abbfc92d5aaa87c32
Alexander Zuykov <web.zuykov@gmail.com>
2023-04-10 22:32:33 +0300

    Обновление сборки

--- src/homework-1/index.ts
+++ src/homework-1/index.ts
@@ -1,24 +1,24 @@
 import { createBitAccessor } from './bit-accessor'
 
 export function homework_1() {
-  const bitAccessor = createBitAccessor(
-    new Uint8Array([0b0000_1100, 0b0000_1001, 0b1111_1111])
-  )
+    const bitAccessor = createBitAccessor(
+        new Uint8Array([0b0000_1100, 0b0000_1001, 0b1111_1111])
+    )
 
-  try {
-    console.log(bitAccessor.getByte(0)) // 0000_1100
+    try {
+        console.log(bitAccessor.getByte(0)) // 0000_1100
 
-    console.log(bitAccessor.getBit(0, 0)) // 0
-    bitAccessor.setBit(0, 0, 1)
-    console.log(bitAccessor.getBit(0, 0)) // 1
-    
-    console.log(bitAccessor.getByte(0)) // 0000_1100
-    console.log(bitAccessor.getByte(1)) // 0000_1001
+        console.log(bitAccessor.getBit(0, 0)) // 0
+        bitAccessor.setBit(0, 0, 1)
+        console.log(bitAccessor.getBit(0, 0)) // 1
 
-    console.log(bitAccessor.getByte(3)) // Index 3 out of range intArray
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(error.message)
+        console.log(bitAccessor.getByte(0)) // 0000_1100
+        console.log(bitAccessor.getByte(1)) // 0000_1001
+
+        console.log(bitAccessor.getByte(3)) // Index 3 out of range intArray
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message)
+        }
     }
-  }
 }
