e9e372f07e32aa0fa226c6747b54fb8b7aeb0ca8
Alexander Zuykov <web.zuykov@gmail.com>
2023-04-10 23:19:32 +0300

    Рефакторинг и добавление Uint16Array | Uint32Array

---
+++ src/homework-1/utils/bit-accessor-validator.ts
@@ -0,0 +1,26 @@
+import { UintArray } from '../types'
+
+export function validate<T extends UintArray>(
+    intArray: T,
+    intIdx: T[number],
+    bitPosition?: number
+): void {
+    const countBitInInt = intArray.BYTES_PER_ELEMENT * 8
+
+    if (intArray.length === 0) {
+        throw new Error('The intArray is empty')
+    }
+
+    if (intIdx >= intArray.length || intIdx < 0) {
+        throw new Error(`Index ${intIdx} out of range intArray [${intArray}]`)
+    }
+
+    if (
+        bitPosition !== undefined &&
+        (bitPosition < 0 || bitPosition > countBitInInt - 1)
+    ) {
+        throw new Error(
+            `Bit index ${bitPosition} for ${countBitInInt}-bit int out of range`
+        )
+    }
+}
