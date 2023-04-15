35af7495799b9b77de71ea9821953930161c77af
Alexander Zuykov <web.zuykov@gmail.com>
2023-04-15 15:47:19 +0300

    Форматирование и установка webpack

--- src/homework-1/bit-accessor.test.ts
+++ src/homework-1/bit-accessor.test.ts
@@ -1,43 +1,35 @@
 import { createBitAccessor } from './bit-accessor'
 
 describe('Тестироваине 8-bit Acessor', function () {
-    it('Получение бита числа', () => {
-        const accessor8bit = createBitAccessor(
-            new Uint8Array([0b0000_1101, 0b0000_1001])
-        )
-        expect(accessor8bit.getBit(0, 2)).toBe(1)
-        expect(accessor8bit.getBit(1, 1)).toBe(0)
-        expect(accessor8bit.getBit(1, 3)).toBe(1)
-    })
-    it('Исключение при выходе за границы массива или байта', () => {
-        const accessor8bit = createBitAccessor(
-            new Uint8Array([0b0000_1101, 0b0000_1001])
-        )
-        expect(() => accessor8bit.getBit(2, 1)).toThrow()
-        expect(() => accessor8bit.getBit(0, 8)).toThrow()
-        expect(() => accessor8bit.getBit(0, -1)).toThrow()
-    })
-    it('Перезапись бита числа', () => {
-        const accessor8bit = createBitAccessor(
-            new Uint8Array([0b0000_1101, 0b0000_1001])
-        )
-        expect(accessor8bit.getBit(0, 0)).toBe(1)
-        accessor8bit.setBit(0, 1, 0)
-        expect(accessor8bit.getBit(0, 0)).toBe(1)
+  it('Получение бита числа', () => {
+    const accessor8bit = createBitAccessor(new Uint8Array([0b0000_1101, 0b0000_1001]))
+    expect(accessor8bit.getBit(0, 2)).toBe(1)
+    expect(accessor8bit.getBit(1, 1)).toBe(0)
+    expect(accessor8bit.getBit(1, 3)).toBe(1)
+  })
+  it('Исключение при выходе за границы массива или байта', () => {
+    const accessor8bit = createBitAccessor(new Uint8Array([0b0000_1101, 0b0000_1001]))
+    expect(() => accessor8bit.getBit(2, 1)).toThrow()
+    expect(() => accessor8bit.getBit(0, 8)).toThrow()
+    expect(() => accessor8bit.getBit(0, -1)).toThrow()
+  })
+  it('Перезапись бита числа', () => {
+    const accessor8bit = createBitAccessor(new Uint8Array([0b0000_1101, 0b0000_1001]))
+    expect(accessor8bit.getBit(0, 0)).toBe(1)
+    accessor8bit.setBit(0, 1, 0)
+    expect(accessor8bit.getBit(0, 0)).toBe(1)
 
-        expect(accessor8bit.getBit(0, 1)).toBe(0)
-        accessor8bit.setBit(0, 1, 1)
-        expect(accessor8bit.getBit(0, 1)).toBe(1)
+    expect(accessor8bit.getBit(0, 1)).toBe(0)
+    accessor8bit.setBit(0, 1, 1)
+    expect(accessor8bit.getBit(0, 1)).toBe(1)
 
-        expect(accessor8bit.getBit(1, 1)).toBe(0)
-        accessor8bit.setBit(1, 1, 1)
-        expect(accessor8bit.getBit(1, 1)).toBe(1)
-    })
-    it('Получение байта числа в виде строки', () => {
-        const accessor8bit = createBitAccessor(
-            new Uint8Array([0b0000_1101, 0b0000_1001])
-        )
-        expect(accessor8bit.getByte(0)).toBe('00001101')
-        expect(accessor8bit.getByte(1)).toBe('00001001')
-    })
+    expect(accessor8bit.getBit(1, 1)).toBe(0)
+    accessor8bit.setBit(1, 1, 1)
+    expect(accessor8bit.getBit(1, 1)).toBe(1)
+  })
+  it('Получение байта числа в виде строки', () => {
+    const accessor8bit = createBitAccessor(new Uint8Array([0b0000_1101, 0b0000_1001]))
+    expect(accessor8bit.getByte(0)).toBe('00001101')
+    expect(accessor8bit.getByte(1)).toBe('00001001')
+  })
 })
--- src/homework-1/bit-accessor.ts
+++ src/homework-1/bit-accessor.ts
@@ -1,29 +1,23 @@
 import { Bit, BitAccessor, UintArray } from './types'
 import { validate } from './utils'
 
-export function createBitAccessor<T extends UintArray>(
-    intArray: T
-): BitAccessor<T> {
-    return {
-        getBit(intIdx, bitPosition) {
-            validate(intArray, intIdx, bitPosition)
-            return ((intArray[intIdx] >> bitPosition) & 0b1) as Bit
-        },
+export function createBitAccessor<T extends UintArray>(intArray: T): BitAccessor<T> {
+  return {
+    getBit(intIdx, bitPosition) {
+      validate(intArray, intIdx, bitPosition)
+      return ((intArray[intIdx] >> bitPosition) & 0b1) as Bit
+    },
 
-        setBit(intIdx, bitPosition, bit) {
-            validate(intArray, intIdx, bitPosition)
-            intArray[intIdx] = (
-                bit
-                    ? intArray[intIdx] | (0b1 << bitPosition)
-                    : intArray[intIdx] & ~(0b1 << bitPosition)
-            ) as Bit
-        },
+    setBit(intIdx, bitPosition, bit) {
+      validate(intArray, intIdx, bitPosition)
+      intArray[intIdx] = (
+        bit ? intArray[intIdx] | (0b1 << bitPosition) : intArray[intIdx] & ~(0b1 << bitPosition)
+      ) as Bit
+    },
 
-        getByte(intIdx) {
-            validate(intArray, intIdx)
-            return intArray[intIdx]
-                .toString(2)
-                .padStart(intArray.BYTES_PER_ELEMENT * 8, '0')
-        },
-    }
+    getByte(intIdx) {
+      validate(intArray, intIdx)
+      return intArray[intIdx].toString(2).padStart(intArray.BYTES_PER_ELEMENT * 8, '0')
+    },
+  }
 }
